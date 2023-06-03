f0c97cce5d75a2375135123c25b64b43a5a2283b
LeeJaeYoung <101277832+dlwodud04@users.noreply.github.com>
2023-06-03 23:07:48 +0900

    23-06-03

---
+++ test12.py
@@ -0,0 +1,16 @@
+from Rect03 import *
+r1 = Rect(3,5)
+print(Rect.count, r1.count, r1.rectid)
+r2 = Rect(4,7)
+print(Rect.count, r2.count, r2.rectid)
+r3 = Rect(6,9)
+print(Rect.count, r3.count, r3.rectid)
+
+#%%
+from Rect03 import *
+r1 = Rect(3,5)
+r2 = Rect(4,7)
+r3 = Rect(6,9)
+print(Rect.count, r1.count, r1.rectid)
+print(Rect.count, r2.count, r2.rectid)
+print(Rect.count, r3.count, r3.rectid)
